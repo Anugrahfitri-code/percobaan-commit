ace5f7a8860f0a4a3db426d790e498b3d3d0c29f
Hanifah Atthahira Basir <hanifahatthahirahbasir@gmail.com>
2025-06-03 13:46:14 +0800

    Add files via upload
    feat: add new method

---
+++ IdeaRepository.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdeaRepository {
+    private static List<Idea> ideas = new ArrayList<>();
+
+    public static void addIdea(Idea idea) {
+        ideas.add(idea);
+    }
+
+    public static List<Idea> getAllIdeas() {
+        return ideas;
+    }
+}
